85df8c33654c6c9fa2375fc67bf4979b6bc88fde
Dusty <dustymccord@gmail.com>
2020-04-21 23:10:52 -0700

    finished section 5.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from "react";
 import ReactDom from "react-dom";
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+      errorMessage: ""
+    };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log(position);
-      },
-      (error) => {
-        console.log(error);
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (error) => this.setState({ errorMessage: error.message })
     );
-    return <div> Latitude: </div>;
+  }
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage} </div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div> Still Loading </div>;
   }
 }
 
